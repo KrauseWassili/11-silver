3725694fe7900278a02de0c60940861344b391a2
Alisher Khamidov <110631572+AlisherKhamidov@users.noreply.github.com>
2025-12-05 14:17:20 +0100

    Revert "169 fix header footer layout"

--- src/components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-dark text-lightest py-3 text-center">
-      <p className="opacity-80">Super-tutor © 2025. All rights reserved.</p>
-    </footer>
-  );
-}
