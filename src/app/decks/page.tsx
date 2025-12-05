38a7106302fdc5aab1a9c35f88e7e437073b8d70
AlexejL <alexejluja@gmail.com>
2025-12-05 12:45:27 +0100

    170-adaptability-page-decks

--- src/app/decks/page.tsx
+++ src/app/decks/page.tsx
@@ -23,33 +23,30 @@ export default async function DecksPage() {
   }));
 
   return (
-    <div className="p-8 bg-(--color-bg) text-(--color-text)">
-      <h2 className="text-4xl font-bold text-center mb-2 text-(--color-primary)">
+    <div className="min-h-screen px-4 py-8 bg-[var(--color-bg)] text-[var(--color-text)]">
+      <h2 className="text-3xl md:text-4xl font-bold text-center mb-10 text-[var(--color-darkest)]">
         Select a deck and tap it to begin training
       </h2>
 
-      <div className="p-20 text-2xl flex-col justify-center space-y-1">
-        {deckWithCount.map((deck) => (
-          <div
-            key={deck.id}
-            className="shadow-lg rounded-xl bg-(--color-bg-secondary) text-(--color-text) 
-                 transition-transform duration-250 hover:scale-101 hover:shadow-xl  
-                 border border-(--color-mid)"
-          >
-            <DeckCard
-              deck={{ id: deck.id, name: deck.title }}
-              count={deck.cardCount}
-            />
-          </div>
-        ))}
-      </div>
+      <div className="max-w-4xl mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 md:gap-3">
+          {deckWithCount.map((deck) => (
+            <div
+              key={deck.id}
+              // className="md:p-2" /* отступ вокруг карточки, чтобы был gap визуально */
+            >
+              <DeckCard
+                deck={{ id: deck.id, name: deck.title }}
+                count={deck.cardCount}
+              />
+            </div>
+          ))}
+        </div>
 
-      <div className="px-24  flex justify-end space-y-10">
-        <div
-          className="shadow-lg rounded-lg bg-(--color-primary) 
-               hover:bg-(--color-primary-hover) transition"
-        >
-          <CreateButton href="/decks/new" label="Create" />
+        <div className="mt-8 flex justify-end">
+          <div className="shadow-lg rounded-lg bg-[var(--color-primary)] hover:bg-[var(--color-primary-hover)] transition">
+            <CreateButton href="/decks/new" label="Create" />
+          </div>
         </div>
       </div>
     </div>
