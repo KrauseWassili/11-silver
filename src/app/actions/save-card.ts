c415907903cb1a5156d5b801a649062bd48bd8e9
WassiliKrause <krausewassili@yandex.com>
2025-12-09 13:18:23 +0100

    feat: added zod to actions for deck edit screen

--- src/app/actions/save-card.ts
+++ src/app/actions/save-card.ts
@@ -3,13 +3,48 @@
 import { db } from "@/db";
 import { flashcards } from "@/db/schema";
 import { updateTag } from "next/cache";
+import z from "zod";
 
-export default async function saveCard(input: {
-  deckId: number;
-  frontText: string;
-  backText: string;
-}) {
-  const { deckId, frontText, backText } = input;
+type ActionState = {
+  errors?: {
+    title?: string[];
+  } | null;
+};
+
+const CardInsertSchema = z.object({
+  frontText: z
+    .string()
+    .trim()
+    .min(1, "Min length must be more than 1")
+    .max(250, "Name is too long, must be under 250"),
+  backText: z
+    .string()
+    .trim()
+    .min(1, "Min length must be more than 1")
+    .max(250, "Name is too long, must be under 250"),
+});
+
+export type EventFormState = {
+  errors?: Record<string, string[]>;
+};
+
+export default async function saveCard(prevState: ActionState,
+  formData: FormData
+): Promise<EventFormState | any> {
+  const deckId = Number(formData.get("deckId"));
+  const frontText = String(formData.get("frontText") ?? "");
+  const backText = String(formData.get("backText") ?? "");
+  const result = CardInsertSchema.safeParse({ frontText, backText });
+
+  if (!result.success) {
+    const errors: Record<string, string[]> = {};
+    for (const issue of result.error.issues) {
+      const field = String(issue.path[0]);
+      if (!errors[field]) errors[field] = [];
+      errors[field].push(issue.message);
+    }
+    return { errors };
+  }
 
   const inserted = await db
     .insert(flashcards)
